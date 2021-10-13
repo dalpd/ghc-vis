883ac3ac38a3815dd8a17d0f920fba9a3f1d54d7
Deniz Alp <denizalpd@ogr.iu.edu.tr>
2021-10-13 21:52:57 +0300

    Add a mock executable to be able to call wrapProgram

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+-- | This is a mock executable to add runtime dependencies to the nix package.
+module Main
+  ( main
+  )
+where
+
+------------------------------------------------------------------------------
+main :: IO ()
+main = pure ()
